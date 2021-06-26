205801bc01f1a944fd09cb4455bb2534cfe8e434
Philipp Bausch <bauschp@informatik.uni-freiburg.de>
2021-06-26 15:13:31 +0200

    Fixed typo in name. added primitive imgui window.

--- src/EyelashVisualizer.cpp
+++ src/EyelashVisualizer.cpp
@@ -10,16 +10,17 @@
 #include <Magnum/Magnum.h>
 #include <Magnum/Shaders/FlatGL.h>
 
+#include <Magnum/ImGuiIntegration/Context.hpp>
 
 #include "EyelashTessellationShader.h"
 
 using namespace Magnum;
 using namespace Math::Literals;
 
-class EyelashVisializer : public Platform::Application
+class EyelashVisualizer : public Platform::Application
 {
 public:
-  explicit EyelashVisializer(const Arguments& arguments);
+  explicit EyelashVisualizer(const Arguments& arguments);
   void drawEvent() override;
 private:
   // Keyboard
@@ -29,12 +30,20 @@ private:
   void mousePressEvent(MouseEvent& event) override;
   void mouseReleaseEvent(MouseEvent& event) override;
   void mouseMoveEvent(MouseMoveEvent& event) override;
+  void viewportEvent(ViewportEvent& event) override;
+
+  // Imgui
+  ImGuiIntegration::Context m_imgui{NoCreate};
+
   GL::Buffer m_singleHairBuffer{NoCreate};
   GL::Mesh m_singleHair{NoCreate};
   GL::Mesh m_singleHairLine{NoCreate};
   EyelashTessellationShader m_coloredShader;
   Shaders::FlatGL3D m_flatColorShader;
   Matrix4 m_transformation, m_projection;
+
+  // Uniforms/Toggles
+  Color3 m_hairColor = 0xc7cf2f_rgbf;
 
   // Camera
   Vector2i m_previousMousePosition;  // used to rotate the view.
@@ -48,12 +57,22 @@ private:
     bool backward;
     bool left;
     bool right;
-  } m_input;
+  } m_input = {};
 };
 
 
-EyelashVisializer::EyelashVisializer(const Arguments& arguments) : Platform::Application{arguments, Configuration{}.setTitle("Eye Lash Visualizer")}
+EyelashVisualizer::EyelashVisualizer(const Arguments& arguments) : Platform::Application{arguments,
+  Configuration{}.setTitle("Eye Lash Visualizer").setWindowFlags(Configuration::WindowFlag::Resizable)}
 {
+
+  m_imgui = ImGuiIntegration::Context(Vector2{windowSize()}/dpiScaling(),
+        windowSize(), framebufferSize());
+
+  GL::Renderer::setBlendEquation(GL::Renderer::BlendEquation::Add,
+  GL::Renderer::BlendEquation::Add);
+  GL::Renderer::setBlendFunction(GL::Renderer::BlendFunction::SourceAlpha,
+  GL::Renderer::BlendFunction::OneMinusSourceAlpha);
+
   GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
   GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
   GL::Renderer::setClearColor(0x0054AA_rgbf);
@@ -66,20 +85,22 @@ EyelashVisializer::EyelashVisializer(const Arguments& arguments) : Platform::App
     float hairWidth;
   } hair[] =
   {
+    {{0.1f, -1.2f, 0.0f},  0.00f},  // control point
     {{0.0f, -1.0f, 0.0f},  0.02f},
     {{-0.1f, -0.8f, 0.0f}, 0.015f},
     {{0.3f, -0.8f, 0.2f},  0.015f},
     {{0.2f, -0.2f, -0.2f}, 0.015f},
     {{0.6f,  0.2f, 0.0f},  0.01f},
-    {{0.3f,  1.0f, 0.0f},  0.0f}
+    {{0.3f,  1.0f, 0.0f},  0.0f},
+    {{0.0f,  1.8f, 0.0f},  0.0f}  // control point
   };
   UnsignedByte indices[] = 
   {
-    0, 0, 1, 2,
     0, 1, 2, 3,
     1, 2, 3, 4,
     2, 3, 4, 5,
-    3, 4, 5, 5,
+    3, 4, 5, 6,
+    4, 5, 6, 7
   };
 
   m_singleHairBuffer = GL::Buffer{};
@@ -103,12 +124,43 @@ EyelashVisializer::EyelashVisializer(const Arguments& arguments) : Platform::App
   m_projection =
     Matrix4::perspectiveProjection(
         35.0_degf, Vector2{windowSize()}.aspectRatio(), 0.01f, 100.0f);
+
+  // setMinimalLoopPeriod(16);
 }
 
 
-void EyelashVisializer::drawEvent()
+void EyelashVisualizer::viewportEvent(ViewportEvent& event)
+{
+  GL::defaultFramebuffer.setViewport({{}, event.framebufferSize()});
+
+  m_imgui.relayout(Vector2{event.windowSize()}/event.dpiScaling(),
+      event.windowSize(), event.framebufferSize());
+}
+
+
+
+void EyelashVisualizer::drawEvent()
 {
   GL::defaultFramebuffer.clear(GL::FramebufferClear::Color|GL::FramebufferClear::Depth);
+
+      /* Enable text input, if needed */
+  if(ImGui::GetIO().WantTextInput && !isTextInputActive())
+      startTextInput();
+  else if(!ImGui::GetIO().WantTextInput && isTextInputActive())
+      stopTextInput();
+  float _floatValue;
+  m_imgui.newFrame();
+  {
+    ImGui::Text("Hello, world!");
+    ImGui::SliderFloat("Float", &_floatValue, 0.0f, 1.0f);
+    ImGui::ColorEdit3("Clear Color", m_hairColor.data());
+//     if(ImGui::Button("Test Window"))
+//         _showDemoWindow ^= true;
+//     if(ImGui::Button("Another Window"))
+//         _showAnotherWindow ^= true;
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+        1000.0/Double(ImGui::GetIO().Framerate), Double(ImGui::GetIO().Framerate));
+  }
 
   Vector3 cameraDirection;
   // forward/backward movement:
@@ -123,17 +175,36 @@ void EyelashVisializer::drawEvent()
 
   GL::Renderer::setPatchVertexCount(4);
 
-  m_coloredShader.setColor(0xc7cf2f_rgbf).setTransformationMatrix(m_view*m_transformation).setProjectionMatrix(m_projection).draw(m_singleHair);
+  m_coloredShader.setColor(m_hairColor).setTransformationMatrix(m_view*m_transformation).setProjectionMatrix(m_projection).draw(m_singleHair);
 
   m_flatColorShader.setColor(0xFF0000_rgbf).setTransformationProjectionMatrix(m_projection*m_view*m_transformation).draw(m_singleHairLine);
+
+
+  /* Set appropriate states. If you only draw ImGui, it is sufficient to
+       just enable blending and scissor test in the constructor. */
+    GL::Renderer::enable(GL::Renderer::Feature::Blending);
+    GL::Renderer::enable(GL::Renderer::Feature::ScissorTest);
+    GL::Renderer::disable(GL::Renderer::Feature::FaceCulling);
+    GL::Renderer::disable(GL::Renderer::Feature::DepthTest);
+
+    m_imgui.drawFrame();
+
+    /* Reset state. Only needed if you want to draw something else with
+       different state after. */
+    GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+    GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+    GL::Renderer::disable(GL::Renderer::Feature::ScissorTest);
+    GL::Renderer::disable(GL::Renderer::Feature::Blending);
+
 
   swapBuffers();
   redraw();
 }
 
 
-void EyelashVisializer::keyPressEvent(KeyEvent& event)
+void EyelashVisualizer::keyPressEvent(KeyEvent& event)
 {
+  if (m_imgui.handleKeyPressEvent(event)) return;
   switch (event.key())
   {
     case KeyEvent::Key::W:
@@ -152,8 +223,9 @@ void EyelashVisializer::keyPressEvent(KeyEvent& event)
       break;
   }
 }
-void EyelashVisializer::keyReleaseEvent(KeyEvent& event)
+void EyelashVisualizer::keyReleaseEvent(KeyEvent& event)
 {
+  if (m_imgui.handleKeyReleaseEvent(event)) return;
   switch (event.key())
   {
     case KeyEvent::Key::W:
@@ -174,19 +246,22 @@ void EyelashVisializer::keyReleaseEvent(KeyEvent& event)
   }
 }
 
-void EyelashVisializer::mousePressEvent(MouseEvent& event)
+void EyelashVisualizer::mousePressEvent(MouseEvent& event)
 {
+  if (m_imgui.handleMousePressEvent(event)) return;
   if((event.button() == MouseEvent::Button::Left))
   {
     m_previousMousePosition = event.position();
   }
 }
-void EyelashVisializer::mouseReleaseEvent(MouseEvent& /*event*/)
+void EyelashVisualizer::mouseReleaseEvent(MouseEvent& event)
 {
+  if (m_imgui.handleMouseReleaseEvent(event)) return;
   redraw();
 }
-void EyelashVisializer::mouseMoveEvent(MouseMoveEvent& event)
+void EyelashVisualizer::mouseMoveEvent(MouseMoveEvent& event)
 {
+  if (m_imgui.handleMouseMoveEvent(event)) return;
   if ((event.buttons() & MouseMoveEvent::Button::Left))
   {
     const Vector2 delta = 3.0f *
@@ -198,4 +273,4 @@ void EyelashVisializer::mouseMoveEvent(MouseMoveEvent& event)
   }
 }
 
-MAGNUM_APPLICATION_MAIN(EyelashVisializer)
+MAGNUM_APPLICATION_MAIN(EyelashVisualizer)
